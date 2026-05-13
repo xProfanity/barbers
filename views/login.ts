f9a93189c3e02e54c905c73c221844df12d23b7c
xprofanity <pecpalmplay@gmail.com>
2026-05-13 12:00:54 +0200

    added base styles css rules

--- views/login.ts
+++ views/login.ts
@@ -1,5 +1,12 @@
 const loginPage = () => `
-	<div>This is a login page!</div>
+	<div class="container">
+		<h1>This is a header</h1>
+		<p>This is a paragraph</p>
+		<label>This is a label</label>
+		<a>This is a link</a>
+		<br />
+		<button>Dont click</button>
+	</div>
 `
 
 export default loginPage
